d720e2c6479562a8a0f72dbb9244910ff2f38e2c
huanghui <1224991097@qq.com>
2023-12-28 15:44:14 +0800

    create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_web_webgl_wrong
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Image.asset(
+            '20231225-161458.jpg',
+          ),
+        ),
+      ),
+    );
+  }
+}
